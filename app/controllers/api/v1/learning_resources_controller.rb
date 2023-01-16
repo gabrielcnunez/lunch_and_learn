bc39340f0e58fd330a68ece6fd4e36490ffd3e11
Gabe Nunez <gabriel.c.nunez@gmail.com>
2023-01-16 12:14:16 -0700

    Build out controller with facades and serializer method calls

--- app/controllers/api/v1/learning_resources_controller.rb
+++ app/controllers/api/v1/learning_resources_controller.rb
@@ -1,5 +1,8 @@
 class Api::V1::LearningResourcesController < ApplicationController
   def search
-    
+    country = params[:country]
+    video = VideoFacade.get_video(country)
+    images = ImagesFacade.get_images(country)
+    render json: LearningResourceSerializer.new(video, images)
   end
 end
