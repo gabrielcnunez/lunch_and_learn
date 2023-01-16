e201de2b5b5699f84f7cfad8863aa060bb54530d
Gabe Nunez <gabriel.c.nunez@gmail.com>
2023-01-16 12:49:15 -0700

    Create videos services to search youtube api by country

---
+++ app/services/videos_service.rb
@@ -0,0 +1,18 @@
+class VideosService
+  def self.get_videos(country)
+    response = conn.get('/youtube/v3/search') do |f|
+      f.params['part']      = 'snippet'
+      f.params['channelId'] = 'UCluQ5yInbeAkkeCndNnUhpw'
+      f.params['q']         = country
+      f.params['type']      = 'video'
+    end
+    require 'pry'; binding.pry
+    JSON.parse(response.body,symbolize_names: true)
+  end 
+
+  def self.conn 
+    Faraday.new('https://www.googleapis.com') do |f|
+      f.params['key'] = ENV['video_api_key']
+    end 
+  end 
+end
