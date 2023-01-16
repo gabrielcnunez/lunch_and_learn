638869ec887669c6bb1851afc72c7b8acd5e52b9
Gabe Nunez <gabriel.c.nunez@gmail.com>
2023-01-16 14:46:17 -0700

    Refactor testing using webmock to stub api calls

--- app/facades/video_facade.rb
+++ app/facades/video_facade.rb
@@ -1,7 +1,6 @@
 class VideoFacade
   def self.get_video(country)
     videos = VideosService.get_videos(country)
-    require 'pry'; binding.pry
     videos[:items].map do |video|
       Video.new(video)
     end.first
--- spec/requests/api/v1/get_learning_resources_spec.rb
+++ spec/requests/api/v1/get_learning_resources_spec.rb
@@ -1,47 +1,48 @@
 require 'rails_helper'
 
 describe 'The Learning Resources API' do
+  let(:response_body_1) { File.open('./spec/fixtures/sample_json/laos_video_search.json')}
+
   it 'can find learning resources based on the name of a country' do
-    # VCR.use_cassette('get_learning_resources_for_thailand') do
-      country = 'thailand'
+    stub_request(:get, /googleapis/).to_return(status: 200, body: response_body_1)
+    country = 'laos'
 
-      get "/api/v1/learning_resources?country=#{country}"
+    get "/api/v1/learning_resources?country=#{country}"
 
-      expect(response).to be_successful
+    expect(response).to be_successful
 
-      resources = JSON.parse(response.body,symbolize_names: true)
+    resources = JSON.parse(response.body,symbolize_names: true)
 
-      expect(resources).to be_a(Hash)
-      expect(resources).to have_key(:data)
-      expect(resources[:data]).to be_an(Array)
+    expect(resources).to be_a(Hash)
+    expect(resources).to have_key(:data)
+    expect(resources[:data]).to be_an(Array)
 
-      resources[:data].each do |resource|
-        expect(resource).to have_key(:id)
-        expect(resource[:id]).to eq(nil)
+    resources[:data].each do |resource|
+      expect(resource).to have_key(:id)
+      expect(resource[:id]).to eq(nil)
 
-        expect(resource).to have_key(:type)
-        expect(resource[:type]).to eq("learning_resource")
+      expect(resource).to have_key(:type)
+      expect(resource[:type]).to eq("learning_resource")
 
-        expect(resource).to have_key(:attributes)
-        expect(resource[:attributes]).to have_key(:country)
-        expect(resource[:attributes][:country]).to eq(country)
+      expect(resource).to have_key(:attributes)
+      expect(resource[:attributes]).to have_key(:country)
+      expect(resource[:attributes][:country]).to eq(country)
 
-        expect(resource[:attributes]).to have_key(:video)
-        expect(resource[:attributes][:video]).to be_a(Hash)
-        expect(resource[:attributes][:video]).to have_key(:title)
-        expect(resource[:attributes][:video]).to have_key(:youtube_video_id)
-        
-        expect(resource[:attributes]).to have_key(:images)
-        expect(resource[:attributes][:images]).to be_an(Array)
+      expect(resource[:attributes]).to have_key(:video)
+      expect(resource[:attributes][:video]).to be_a(Hash)
+      expect(resource[:attributes][:video]).to have_key(:title)
+      expect(resource[:attributes][:video]).to have_key(:youtube_video_id)
+      
+      expect(resource[:attributes]).to have_key(:images)
+      expect(resource[:attributes][:images]).to be_an(Array)
 
-        resource[:attributes][:images].each do |image|
-          expect(image).to have_key(:alt_tag)
-          expect(image).to have_key(:url)
-          expect(image.keys.size).to eq(2)
-        end
-
-        expect(resource[:attributes].keys.size).to eq(3)
+      resource[:attributes][:images].each do |image|
+        expect(image).to have_key(:alt_tag)
+        expect(image).to have_key(:url)
+        expect(image.keys.size).to eq(2)
       end
-    # end
+
+      expect(resource[:attributes].keys.size).to eq(3)
     end
   end
+end
