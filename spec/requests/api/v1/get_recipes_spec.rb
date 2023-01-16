9d5a68e5aa6985877d74e476c668375cfe029397
Gabe Nunez <gabriel.c.nunez@gmail.com>
2023-01-15 22:38:08 -0700

    Fix random country test to use vcr for all countries and stub json response

--- spec/requests/api/v1/get_recipes_spec.rb
+++ spec/requests/api/v1/get_recipes_spec.rb
@@ -23,41 +23,43 @@ describe 'The Recipes API' do
         expect(recipe[:type]).to eq("recipe")
 
         expect(recipe).to have_key(:attributes)
-        expect(recipe[:data][0][:attributes]).to have_key(:title)
-        expect(recipe[:data][0][:attributes]).to have_key(:url)
-        expect(recipe[:data][0][:attributes]).to have_key(:country)
-        expect(recipe[:data][0][:attributes][:country]).to eq(country)
-        expect(recipe[:data][0][:attributes]).to have_key(:image)
-        expect(recipe[:data][0][:attributes].keys.size).to eq(4)
+        expect(recipe[:attributes]).to have_key(:title)
+        expect(recipe[:attributes]).to have_key(:url)
+        expect(recipe[:attributes]).to have_key(:country)
+        expect(recipe[:attributes][:country]).to eq(country)
+        expect(recipe[:attributes]).to have_key(:image)
+        expect(recipe[:attributes].keys.size).to eq(4)
       end
     end
   end
 
   it 'can find recipes based on a random country' do
-    VCR.use_cassette('get_recipes_from_random_country') do
-      get "/api/v1/recipes"
-
-      expect(response).to be_successful
-
-      recipes = JSON.parse(response.body,symbolize_names: true)
-
-      expect(recipes).to be_a(Hash)
-      expect(recipes).to have_key(:data)
-      expect(recipes[:data]).to be_an(Array)
-      
-      expect(recipes[:data][0]).to have_key(:id)
-      expect(recipes[:data][0][:id]).to eq(nil)
-
-      expect(recipes[:data][0]).to have_key(:type)
-      expect(recipes[:data][0][:type]).to eq('recipe')
-
-      expect(recipes[:data][0]).to have_key(:attributes)
-      expect(recipes[:data][0][:attributes]).to have_key(:title)
-      expect(recipes[:data][0][:attributes]).to have_key(:url)
-      expect(recipes[:data][0][:attributes]).to have_key(:country)
-      expect(recipes[:data][0][:attributes]).to have_key(:image)
-      expect(recipes[:data][0][:attributes].keys.size).to eq(4)
+    VCR.use_cassette('all_countries') do
+      json_response = File.read('spec/fixtures/sample_json/some_random_country.json')
+      stub_request(:any, /edamam/).to_return(status: 200, body: json_response)
+      get '/api/v1/recipes'
     end
+
+    expect(response).to be_successful
+
+    recipes = JSON.parse(response.body,symbolize_names: true)
+
+    expect(recipes).to be_a(Hash)
+    expect(recipes).to have_key(:data)
+    expect(recipes[:data]).to be_an(Array)
+    
+    expect(recipes[:data][0]).to have_key(:id)
+    expect(recipes[:data][0][:id]).to eq(nil)
+
+    expect(recipes[:data][0]).to have_key(:type)
+    expect(recipes[:data][0][:type]).to eq('recipe')
+
+    expect(recipes[:data][0]).to have_key(:attributes)
+    expect(recipes[:data][0][:attributes]).to have_key(:title)
+    expect(recipes[:data][0][:attributes]).to have_key(:url)
+    expect(recipes[:data][0][:attributes]).to have_key(:country)
+    expect(recipes[:data][0][:attributes]).to have_key(:image)
+    expect(recipes[:data][0][:attributes].keys.size).to eq(4)
   end
 
   it 'returns an empty array when no recipes match the chosen country' do
