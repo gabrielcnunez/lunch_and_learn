c82b6a3ef2b81ce2ccd42414181cc0f425b53768
Gabe Nunez <gabriel.c.nunez@gmail.com>
2023-01-17 13:24:01 -0700

    Create favorite model, add validations and relationships to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :favorites
   validates :name, :email, :api_key,presence: true
   validates_uniqueness_of :email, case_sensitive: false
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 describe User, type: :model do
+  describe 'relationships' do 
+    it { should have_many :favorites }
+  end
+
   describe 'validations' do
     it { should validate_presence_of :name }
     it { should validate_presence_of :email }
