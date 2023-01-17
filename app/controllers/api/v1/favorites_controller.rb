e461580e9357af68ec67c33a8dbaff3bf43a902f
Gabe Nunez <gabriel.c.nunez@gmail.com>
2023-01-17 15:40:33 -0700

    Refactor to add before_action for find user, build out index response

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -1,17 +1,30 @@
 class Api::V1::FavoritesController < ApplicationController
+  before_action :find_user
+  
+  def index
+    if @user.nil?
+      render json: ErrorSerializer.invalid_key, status: :bad_request
+    else
+      render json: FavoriteSerializer(@user.favorites)
+    end
+  end
+  
   def create
-    user = User.find_by(api_key: params[:api_key])
-    if user.nil?
+    if @user.nil?
       render json: ErrorSerializer.invalid_key, status: :bad_request
     else
       favorite = Favorite.new(favorite_params)
-      favorite.user_id = user.id
+      favorite.user_id = @user.id
       favorite.save
       render json: { success: "Favorite added successfully" }, status: :created
     end
   end
 
   private
+
+    def find_user
+      @user = User.find_by(api_key: params[:api_key])
+    end
 
     def favorite_params
       params.require(:favorite).permit(:country, :recipe_link, :recipe_title)
