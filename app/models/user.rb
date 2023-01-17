2355fd397387f706b913fe98db0712705b07a142
Gabe Nunez <gabriel.c.nunez@gmail.com>
2023-01-16 23:43:29 -0700

    Add validations to user model, pass testing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  # before_create :assign_api_key
+  validates :name, :email, :api_key, presence: true
+  validates_uniqueness_of :email, case_sensitive: false
+
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe User, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :email }
+    it { should validate_presence_of :api_key }
+    it { should validate_uniqueness_of(:email).ignoring_case_sensitivity }
+  end
 end
