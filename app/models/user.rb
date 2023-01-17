a3a07233e0eaa96c78be2bc4269f5062c2af7884
Gabe Nunez <gabriel.c.nunez@gmail.com>
2023-01-17 00:43:24 -0700

    Move api_key generation from model to controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,4 @@
 class User < ApplicationRecord
-  # before_create :assign_api_key
-  validates :name, :email, :api_key, presence: true
+  validates :name, :email, :api_key,presence: true
   validates_uniqueness_of :email, case_sensitive: false
-
-
 end
